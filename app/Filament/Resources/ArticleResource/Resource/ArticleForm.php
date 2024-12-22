ea279f6b4823533fc2f4d209587c38ad89739034
mouayed keziz <m_keziz@estin.dz>
2024-12-22 19:17:24 +0100

    -

--- app/Filament/Resources/ArticleResource/Resource/ArticleForm.php
+++ app/Filament/Resources/ArticleResource/Resource/ArticleForm.php
@@ -4,6 +4,9 @@ namespace App\Filament\Resources\ArticleResource\Resource;
 
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Group;
+use Illuminate\Support\Str;
 
 class ArticleForm
 {
@@ -11,16 +14,70 @@ class ArticleForm
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->required(),
-                Forms\Components\Textarea::make('description')
-                    ->required()
-                    ->columnSpanFull(),
-                Forms\Components\Textarea::make('content')
-                    ->required()
-                    ->columnSpanFull(),
-                Forms\Components\DateTimePicker::make('published_at')
-                    ->label('Publish Date'),
-            ]);
+                Group::make()
+                    ->columnSpan(['lg' => 2])
+                    ->schema([
+                        Section::make('Article Information')
+                            ->columns(2)
+                            ->schema([
+                                Forms\Components\TextInput::make('title')
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->live(onBlur: true)
+                                    ->afterStateUpdated(fn(string $state, Forms\Set $set) => $set('slug', Str::slug($state))),
+
+                                Forms\Components\TextInput::make('slug')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required(),
+
+                                Forms\Components\Textarea::make('description')
+                                    ->required()
+                                    ->rows(4)
+                                    ->columnSpanFull(),
+
+                                Forms\Components\RichEditor::make('content')
+                                    ->required()
+                                    ->columnSpanFull(),
+
+                                Forms\Components\DateTimePicker::make('published_at')
+                                    ->label(__('articles.form.published_date'))
+                                    ->columnSpanFull(),
+                            ]),
+                    ]),
+
+                Group::make()
+                    ->columnSpan(['lg' => 1])
+                    ->schema([
+                        Section::make('Featured Image')
+                            ->schema([
+                                Forms\Components\FileUpload::make('image')
+                                    ->image()
+                                    ->directory('articles')
+                                    ->columnSpanFull(),
+                            ]),
+
+                        Section::make(__('articles.categories.plural'))
+                            ->schema([
+                                Forms\Components\Select::make('categories')
+                                    ->multiple()
+                                    ->relationship('categories', 'name')
+                                    ->createOptionForm([
+                                        Forms\Components\TextInput::make('name')
+                                            ->required()
+                                            ->maxLength(255)
+                                            ->live(onBlur: true)
+                                            ->afterStateUpdated(fn(string $state, Forms\Set $set) => $set('slug', Str::slug($state))),
+
+                                        Forms\Components\TextInput::make('slug')
+                                            ->disabled()
+                                            ->dehydrated()
+                                            ->required(),
+                                    ])
+                                    ->columnSpanFull(),
+                            ]),
+                    ]),
+            ])
+            ->columns(['lg' => 3]);
     }
 }
