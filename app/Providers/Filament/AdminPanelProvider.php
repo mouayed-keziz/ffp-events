dc5d6bb2d0775948d3b1d180186d84cdc42ddd70
Mouayed Keziz <m_keziz@estin.dz>
2025-02-14 18:49:32 +0100

    -

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers\Filament;
 
 use App\Filament\Resources\EventAnnouncementResource\Widgets\EventAnnouncementAdvancedStats;
 use App\Filament\Resources\EventAnnouncementResource\Widgets\EventAnnouncementStats;
-use App\Filament\Resources\UserResource\Widgets\ExhibitorStats;
+// use App\Filament\Resources\UserResource\Widgets\ExhibitorStats;
 use App\Filament\Resources\VisitorResource\Widgets\UserStats;
 use DutchCodingCompany\FilamentDeveloperLogins\FilamentDeveloperLoginsPlugin;
 use Filament\Http\Middleware\Authenticate;
@@ -66,11 +66,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                EventAnnouncementStats::class,
-                UserStats::class,
-                ExhibitorStats::class,
-                EventAnnouncementAdvancedStats::class,
-                // Widgets\AccountWidget::class,
+                // EventAnnouncementStats::class,
+                // UserStats::class,
+                // ExhibitorStats::class,
+                // EventAnnouncementAdvancedStats::class,
+                Widgets\AccountWidget::class,
                 // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
