41dd056ed50cc88b404f314a3c9b2de7338162d1
Mouayed Keziz <m_keziz@estin.dz>
2025-03-21 18:49:45 +0100

    -

---
+++ app/Infolists/Components/RadioPricedEntry.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Infolists\Components;
+
+use Filament\Infolists\Components\Entry;
+
+class RadioPricedEntry extends Entry
+{
+    protected string $view = 'panel.components.infolist.radio-priced-display';
+}
---
+++ app/Infolists/Components/SelectPricedEntry.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Infolists\Components;
+
+use Filament\Infolists\Components\Entry;
+
+class SelectPricedEntry extends Entry
+{
+    protected string $view = 'panel.components.infolist.select-priced-display';
+}
