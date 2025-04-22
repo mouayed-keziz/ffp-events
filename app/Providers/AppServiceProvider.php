9ebb34c4f6778277e2c6e5b14866056fe01e9504
Mouayed Keziz <m_keziz@estin.dz>
2025-04-22 12:14:51 +0100

    -

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // Add this line to suppress the deprecation warning
         error_reporting(E_ALL ^ E_DEPRECATED);
-
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch->locales(['ar', 'en', 'fr']);
         });
