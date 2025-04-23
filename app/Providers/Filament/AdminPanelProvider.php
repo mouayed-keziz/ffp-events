19744811a716a5e49615d52c010135dcf0e3670c
Mouayed Keziz <91923112+mouayed-keziz@users.noreply.github.com>
2025-04-23 18:15:01 +0100

    -

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -67,9 +67,9 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 // EventAnnouncementStats::class,
-                // UserStats::class,
-                // ExhibitorStats::class,
-                // EventAnnouncementAdvancedStats::class,
+                 UserStats::class,
+                 ExhibitorStats::class,
+                 EventAnnouncementAdvancedStats::class,
                 Widgets\AccountWidget::class,
                 // Widgets\FilamentInfoWidget::class,
             ])
