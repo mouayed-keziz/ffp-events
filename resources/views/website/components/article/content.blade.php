9545c92590801068db5a07c7c4ca75d871823c06
Mouayed Keziz <m_keziz@estin.dz>
2025-04-25 18:22:30 +0100

    -

--- resources/views/website/components/article/content.blade.php
+++ resources/views/website/components/article/content.blade.php
@@ -5,16 +5,19 @@
 </div>
 
 <div class="container mx-auto px-4 py-8">
-    <hr class="border-y-1 border-dashed border-gray-300">
-    <div class="flex justify-between items-center my-6">
-        @include('website.components.article.tags', ['tags' => $article->categories])
-        @livewire('website.share', [
-            'id' => 'content',
-            'title' => $article->title,
-            'description' => $article->description,
-            'url' => route('article', $article),
-            'model' => $article,
-        ])
-    </div>
-    <hr class="border-t-1 border-dashed border-gray-300">
+    @if ($article->categories->count() > 0)
+        <hr class="border-y-1 border-dashed border-gray-300">
+        <div class="flex justify-between items-center my-6">
+            @include('website.components.article.tags', ['tags' => $article->categories])
+
+            @livewire('website.share', [
+                'id' => 'content',
+                'title' => $article->title,
+                'description' => $article->description,
+                'url' => route('article', $article),
+                'model' => $article,
+            ])
+        </div>
+        <hr class="border-t-1 border-dashed border-gray-300">
+    @endif
 </div>
