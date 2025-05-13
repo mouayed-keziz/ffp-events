fa43fbf61523a754aa789fd849a75f295e8442d2
Mouayed Keziz <m_keziz@estin.dz>
2025-05-13 06:20:58 +0100

    -

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -70,8 +70,8 @@ class AdminPanelProvider extends PanelProvider
             // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 GeneralStatsOverview::class,
-                // VisitorSubmissionsPerEventChart::class,
-                // ExhibitorSubmissionsPerEventChart::class,
+                VisitorSubmissionsPerEventChart::class,
+                ExhibitorSubmissionsPerEventChart::class,
                 VisitorSubmissionsByEventOverTimeChart::class, // Added widget
                 ExhibitorSubmissionsByEventOverTimeChart::class, // Added widget
                 // UserStats::class,
