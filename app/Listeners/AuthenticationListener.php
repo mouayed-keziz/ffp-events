bd0e87f324bfb573033990f8aa0c81a041a190c5
Mouayed Keziz <m_keziz@estin.dz>
2025-07-13 12:38:08 +0100

    -

--- app/Listeners/AuthenticationListener.php
+++ app/Listeners/AuthenticationListener.php
@@ -25,14 +25,14 @@ class AuthenticationListener
     public function handle(object $event): void
     {
         if ($event instanceof Login) {
-            AuthenticationActivity::logLogin($event->user);
+            // AuthenticationActivity::logLogin($event->user);
         }
 
         if ($event instanceof Logout) {
             // Fix for logout event where user might be null
             // For the "visitor" guard, the user might be null in the logout event
             // We don't log anything in that case as the AuthenticationActivity handles the null check
-            AuthenticationActivity::logLogout($event->user);
+            // AuthenticationActivity::logLogout($event->user);
         }
     }
 }
