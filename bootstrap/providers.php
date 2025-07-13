0c6e7a77bf0af77e98e4894cff3eacb3d5a91a1c
Mouayed Keziz <m_keziz@estin.dz>
2025-07-13 17:49:59 +0100

    -

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,7 +4,8 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class,
     Orangehill\Iseed\IseedServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
 ];
