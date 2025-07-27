0f8b95b2b54921a2a34decd1b5cc2fce1a2d3387
Mouayed Keziz <m_keziz@estin.dz>
2025-07-27 14:47:47 +0100

    -

--- resources/views/website/pages/events/visit-event.blade.php
+++ resources/views/website/pages/events/visit-event.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include('website.components.visit-event.banner', ['event' => $event])
     <main class="w-full max-w-5xl mx-auto px-2 md:px-4 py-8">
-        <div class="-mt-40 relative z-10">
+        <div class="-mt-32 relative z-10">
             <div class="bg-white rounded-xl shadow-lg px-4 md:px-6 py-6">
                 @livewire('website.events.visit-event', ['event' => $event])
             </div>
